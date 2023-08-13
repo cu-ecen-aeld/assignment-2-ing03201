f0b6c16e3f3060a2cf7bb8eb5703a5e251942a72
ing03201 <ing03201@gmail.com>
2023-08-14 00:04:19 +0900

    finder is running in bash

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 if [ -z $1 ] || [ -z $2 ]; then
         echo "Two arguments are required\n first argument is directory path to be searched\n second argument is text string to be searched in first argument path\n"
         exit 1
